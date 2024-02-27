d6cd7b787ed6abbfd57538f67fecfd64a9119e81
Barath005 <barathganesan01@gmail.com>
2024-02-27 11:33:27 +0530

    winning alert

--- scoreInput.js
+++ scoreInput.js
@@ -176,6 +176,7 @@ document.addEventListener('click', e=>{
                 addBowlerRuns(0);
                 displayBall(0);
                 overCount();
+                checkWinner()
                 break;
             case 'one':
                 addBatsmanRuns(1);
@@ -183,6 +184,7 @@ document.addEventListener('click', e=>{
                 addScore(1);
                 displayBall(1);
                 overCount();
+                checkWinner()
                 break;
             case 'two':
                 addBatsmanRuns(2);
@@ -190,6 +192,7 @@ document.addEventListener('click', e=>{
                 addScore(2);
                 displayBall(2);
                 overCount();
+                checkWinner()
                 break;
             case 'three':
                 addBatsmanRuns(3);
@@ -197,6 +200,7 @@ document.addEventListener('click', e=>{
                 addScore(3);
                 displayBall(3);
                 overCount();
+                checkWinner()
                 break;
             case 'four':
                 addBatsmanRuns(4,true);
@@ -204,6 +208,7 @@ document.addEventListener('click', e=>{
                 addScore(4);
                 displayBall(4);
                 overCount();
+                checkWinner()
                 break;
             case 'six':
                 addBatsmanRuns(6,true);
@@ -211,18 +216,23 @@ document.addEventListener('click', e=>{
                 addScore(6);
                 displayBall(6);
                 overCount();
+                checkWinner()
                 break;
             case 'wide':
                 addScore(1);
+                checkWinner()
                 break;
             case 'noball':
                 addScore(1);
+                checkWinner()
                 break;
             case 'bye':
                 overCount();
+                checkWinner()
                 break;
             case 'legbye':
                 overCount();
+                checkWinner()
                 break;
             case 'undo':
 
@@ -230,11 +240,14 @@ document.addEventListener('click', e=>{
             case 'other':
                 addScore(5);
                 overCount();
+                checkWinner()
                 break;
             case 'out':
                 displayBall('W');
                 batsmanOut();
                 overCount();
+                checkWinner()
+                
                 break;
         }
 })
@@ -304,6 +317,7 @@ function addScore(score){
     let result = checkBattingTeam();
     let team = result.team;
     team.totalScore += score;
+    checkWinner();
     localStorage.setItem(`team${result.number}`, JSON.stringify(team));
 }
 
@@ -319,6 +333,9 @@ function overCount(){
     over += .1;
     over = over.toFixed(1);
     over = parseFloat(over);
+    inningsOver()
+    /* checkWinner() */
+    
 
     
     team.completedOvers = over;
@@ -355,6 +372,7 @@ function isCheckOver(){
         localStorage.setItem(`team${result.number}`, JSON.stringify(team));
         // console.log(team1)
         console.log(over);
+        alert("over end")
         ballCount=6;
         swapPlayers();
         overRuns = 0;
@@ -433,6 +451,8 @@ function batsmanOut(){
     let bowlTeam = bowlingTeam.team;
     bowlTeam.player[bowlerId].bowling.wicket += 1; 
     localStorage.setItem(`team${bowlingTeam.number}`, JSON.stringify(bowlTeam));
+    
+    // inningsOver()    
 
     if(!inningsOver()){
 
@@ -524,25 +544,31 @@ function handleRuns(runs , runType) {
         displayBall(`${runs==0?'':runs}WD`);
         team.extra.wide += (1+runs);
         addBatsmanRuns(runs+1);
+        /* checkWinner(); */
     }else if(runType=='Noball'){
         displayBall(`${runs==0?'':runs}NB`);
         team.extra.noBall += (1+runs);
         addBatsmanRuns(runs+1);
+        /* checkWinner() */;
     }else if(runType=='Bye'){
         displayBall(`${runs==0?'':runs}B`);
         team.extra.byes += runs;
         addBatsmanRuns(runs); 
+        /* checkWinner() */;
     }else if(runType=='Legbye'){
         displayBall(`${runs==0?'':runs}B`);
         team.extra.legByes += runs; 
         addBatsmanRuns(runs); 
+        /* checkWinner(); */
     }else{
         addBatsmanRuns(runs);
         displayBall(runs);
+        /* checkWinner(); */
     }
 
     
     addBowlerRuns(runs);
+
 }
 
 // Function to hide the popup box
@@ -553,22 +579,31 @@ function hidePopup() {
 
 // Check if first innings id over
 var matchData = JSON.parse(localStorage.getItem('matchData'));
-
+let i=0
 function inningsOver(){
     let result = checkBattingTeam();
     let result2 = checkBowlingTeam();
     let team = result.team
     let team2 = result2.team
     if((team.totalWickets==matchData.noOfPlayers-1)|| (team.completedOvers==matchData.totalOvers)){
+        if(!team2.halfInnings){
+            innings_popup(team2.teamName,team.totalScore); 
+            
+           
+
+        }
+
         /* alert("Match over"); */
         team.halfInnings = true;
         team.innings = 2;
         team2.innings = 1;
 
         localStorage.setItem(`team${result.number}`, JSON.stringify(team));  
-        localStorage.setItem(`team${result2.number}`, JSON.stringify(team2));  
+        localStorage.setItem(`team${result2.number}`, JSON.stringify(team2));
 
-        innings_popup(team2.teamName,team.totalScore);
+        
+       
+        // innings_popup(team, runs)
 
         return true;
     }
@@ -578,6 +613,8 @@ function inningsOver(){
 
 // Innings alert
 function innings_popup(name, runs) {
+    i=1;
+
     document.querySelector('.Team-name').innerText = name + " Needs " + (runs + 1) + " Runs In " + matchData.totalOvers * 6 + " Balls"
     document.querySelector('.pop-up-box').style.visibility = 'visible'
     document.querySelector('.Innings-Alert').classList.add('active');
@@ -588,7 +625,100 @@ function innings_popup(name, runs) {
     document.getElementById("nextInnings").addEventListener("click",() => {
         window.location.href="selectPlayers.html"
         sessionStorage.clear();
+       
+        alert(i)
     })
 } 
 
 display();
+
+
+//winning alert
+
+
+/* 
+function inningsOver1(){
+
+  if (overs1 === totalOvers || wicketsT1 === noOfPlayers - 1) {
+    T1inningover = true;
+    innings_popup(Team1, runs)
+   }
+  
+  if (overs2 === totalOvers || wicketsT2 === noOfPlayers - 1) {
+    T2inningover = true;
+    innings_popup(Team2, runs)
+  }
+} */
+
+function checkWinner() {
+    var team1Data = JSON.parse(localStorage.getItem('team1'));
+var team2Data = JSON.parse(localStorage.getItem('team2'));
+var matchData = JSON.parse(localStorage.getItem('matchData'));
+
+var Team1 =  team1Data.teamName;
+var Team2 =  team2Data.teamName;
+var runsT1 =  team1Data.totalScore;
+var runsT2 =  team2Data.totalScore;
+var T1inningover =team1Data.halfInnings;
+var T2inningover=team2Data.halfInnings;
+// var overs1 =  team1Data.completedOvers;
+// var overs2 =  team2Data.completedOvers;
+// var wicketsT1 =  team1Data.totalWickets;
+// var wicketsT2 = team2Data.totalWickets ;
+// var totalOvers = matchData.totalOvers ;
+// var noOfPlayers =matchData.noOfPlayers;
+var winnerflag = false;
+
+
+
+    var winname = document.querySelector(".winning-team-name");
+    if (T1inningover && T2inningover) {
+        if (runsT1 > runsT2) {
+            alert("Team 1 wins")
+            winname.innerText = `${Team1}  Won The Match`;
+            winnerflag = true;
+            Winner();
+        }
+        
+
+        else if(runsT2 > runsT1) {
+            alert("Team 2 wins")
+            winname.innerText =  `${Team2}  Won The Match`;
+            winnerflag = true;
+            Winner();
+        }
+
+        else if(runsT1 == runsT2) {
+            alert("Team 1 draw")
+            winname.innerText = "Match Draw";
+            winnerflag = true;
+            Winner();
+        }
+       
+    }
+
+    else if (T1inningover && (runsT2 > runsT1)) {
+        alert("Team 2 win")
+        winname.innerText =  `${Team2}  Won The Match`;
+        winnerflag = true;
+
+        Winner();
+    }
+
+    else if (T2inningover && (runsT1 > runsT2)) {
+        alert("Team 1 wns")
+        winname.innerText =  `${Team1}  Won The Match`;
+        winnerflag = true;
+        Winner();
+    }
+}
+
+function Winner() {
+    document.querySelector('.pop-up-box').style.visibility = 'visible'
+    document.querySelector('.Winner-Alert').classList.add('active');
+    document.querySelectorAll('.close')[5].addEventListener('click', function () {
+        document.querySelector('.Winner-Alert').classList.remove('active');
+        document.querySelector('.pop-up-box').style.visibility = 'hidden'
+    })
+
+}
